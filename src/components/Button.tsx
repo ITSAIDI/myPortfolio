d0592a9bd2fe07e8ea9118a72e03b036af32bab0
ITSAIDI <noureddinesaidi111@gmail.com>
2025-12-19 01:18:31 +0100

    big updates

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+
+type ButtonInput = {
+  handleClick : () => void,
+  text : string,
+  iconClassname ?: string, // Optional params
+  textClassname ?: string,
+  buttonClassname ?: string,
+
+}
+
+export default function Button(props : ButtonInput) {
+  return (
+     <button
+            className={
+            `
+            group
+            text-green1
+            px-3 py-0
+            rounded-full
+            hover:border-green2 
+            transition-all duration-500 ease-in-out
+            cursor-pointer 
+            border-2 border-green1
+            ${props.buttonClassname ?? ''}
+            `
+            }
+            onClick={props.handleClick}
+            >  
+            <span
+            className={`font-nunito font-extrabold ${props.textClassname ?? ''}`}
+            >
+            {props.text} </span>
+            <i className={`
+              transition-transform duration-200 ease-in
+              group-hover:scale-110 ${props.iconClassname ?? ''}`}></i>
+        </button>
+  )
+}
---
+++ src/pages/Hero.tsx
@@ -0,0 +1,76 @@
+import Button from "../components/Button"
+import Photo from "../components/Photo"
+import { Particles } from "../components/ui/particles"
+import TypingText from "../components/ui/typing-text"
+
+// Nunito,Josefin Sans, Futura
+export default function Hero() {
+
+  function resumeBttClick()
+  {
+    window.open("https://drive.google.com/file/d/1pauEf7CQUYQ2LrDnc1vbZVg4LMUYEyte/view?usp=sharing", "_blank");
+  }
+  function contactBttClick()
+  {
+    
+  }
+return (
+    <div className="w-full h-full relative p-7 bg-black1">
+      {/* Animated background */}
+       <Particles
+        className="absolute inset-0"
+        quantity={100}
+        ease={80}
+        staticity={50}
+        color="#00ff99"
+        size={0.8}
+      />
+
+      
+      <div className="flex flex-col lg:flex-row justify-between lg:items-center">
+        <div className="flex flex-col lg:order-1 order-2 gap-6">
+           {/* Hero text*/}
+
+            <div className="lg:text-left text-white font-josefinsans flex flex-col gap-4">
+              <h1 className="text-xl font-bold text-green1 lg:text-4xl">When AI Meets Engineering</h1>
+              <p className="lg:text-lg max-w-[700px]">
+                I design and deploy intelligent solutions that blend advanced machine learning with elegant, 
+                functional interfaces. From data pipelines to production-ready apps.
+              </p>
+
+              <TypingText
+                className="lg:text-2xl font-nunito font-bold"
+                text={["MLOps", "LLMOps", "Data Science", "Mobile & Web Dev"]}
+                typingSpeed={150}
+                initialDelay={50}
+                pauseDuration={2000}
+                loop = {true}
+                showCursor={true}
+                cursorCharacter="✦"
+                textColors={['#ffff']}
+              />
+           </div>
+           {/* Call_To_Action */}
+
+           <div className="">
+            {/* Resume */} <Button text="Resume" handleClick={resumeBttClick}  iconClassname="fa-solid fa-arrow-up-right-from-square text-black1" textClassname="text-black1" buttonClassname="bg-green1 hover:bg-green2"/>
+            {/* Contact*/} <Button text="Contact" handleClick={contactBttClick} buttonClassname="bg-transparent hover:text-green2"/>
+           </div>
+        </div>
+        
+
+        <div className="lg:order-2 order-1">
+            <Photo />
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+{/*
+<p className="mt-3 leading-5 max-w-[300px] text-[10px] lg:text-lg lg:max-w-[600px] lg:leading-8">
+    I build and design AI based solutions in multiple industries and I'm
+    proficient in various technologies and programming languages.
+</p>
+*/}
